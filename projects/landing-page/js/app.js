eb09ee5fbcfb35b9f16413f2a7ab9151f0469ae2
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-10-10 01:13:16 +0200

    adding nav menu - setting active class on sections

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,57 +1,68 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
-
+ *
+ */
+let sections = document.querySelectorAll("section");
+let navList = document.querySelector("#navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
+let liFragment = document.createDocumentFragment();
+sections.forEach((sec) => {
+  let li = document.createElement("li");
+  li.className = "menu__link";
+  li.textContent = sec.querySelector("h2").textContent;
+  liFragment.appendChild(li);
+});
+navList.appendChild(liFragment);
 
 // Add class 'active' to section when near top of viewport
-
+function addActiveNearTop() {
+  sections.forEach((sec) => {
+    secBounds = sec.getBoundingClientRect();
+    if (secBounds.top <= 200 && secBounds.top >= 0) {
+      sections.forEach((sec) => sec.classList.remove("active"));
+      sec.classList.add("active");
+    }
+  });
+}
 
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
+document.addEventListener("scroll", addActiveNearTop);
